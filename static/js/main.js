99c2fc289b5b64a6c46f7b2880d103fc2fbfebe8
David Lohle <Proplex@users.noreply.github.com>
2019-01-21 14:27:03 -0500

    Minor JS issues with toggle

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,17 @@
 function updateTables() {
-  $('tr:visible').each(function(index) {
-    $(this).css(
-        'background-color',
-        !!(index & 1) ? 'rgba(0,0,0,.05)' : 'rgba(0,0,0,0)');
+  $('.table').each(function() {
+    $(this).children('tr:visible').each(function(index) {
+      $(this).css(
+          'background-color',
+          !!(index & 1) ? 'rgba(0,0,0,.05)' : 'rgba(0,0,0,0)');
+    });
   });
 }
 
 function toggleModern() {
-  var modernUI = $('.modern');
   $('.modern').each(function(index) {
     $(this).css('display', $(this).css('display') === 'none' ? 'table-row' : 'none');
   });
+  updateTables();
 }
+window.addEventListener('DOMContentLoaded', updateTables());
