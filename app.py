b7c0ccf8d1ce208e6fb6ceb4e7bfb5e616bcb89d
David Lohle <Proplex@users.noreply.github.com>
2019-01-22 10:49:20 -0500

    Rework data backend to make it mroe API-friendly

--- app.py
+++ app.py
@@ -1,31 +1,63 @@
-import os
-from flask import Flask, render_template, current_app
+import os, json
+from flask import Flask, render_template, current_app, request
 from flask_bootstrap import Bootstrap
+from flask_basicauth import BasicAuth
 from models.data import Data
 
+# get envvars
 DEBUG = os.getenv('DEBUG', False)
+AUTH_USER = os.getenv('BASIC_AUTH_USERNAME', 'bober')
+AUTH_PASS = os.getenv('BASIC_AUTH_PASSWORD', 'pleasechange')
 
+# bootstrap the app
 app = Flask(__name__)
+
+# configure app before libraries
+app.config['BASIC_AUTH_USERNAME'] = AUTH_USER
+app.config['BASIC_AUTH_PASSWORD'] = AUTH_PASS
+
+# attach libraries
 Bootstrap(app)
+basic_auth = BasicAuth(app)
 store = Data()
 
 @app.route('/')
 def index():
   deps = store.get_data()
-  release_list = sorted(deps['releases'], key=lambda k: k['name']) 
-  kit_list = sorted(deps['kits'], key=lambda k: k['name']) 
-  return render_template("index.jinja", releases=release_list, kits=kit_list)
+  print(deps)
+  return render_template("index.jinja", releases=deps['releases'], kits=deps['kits'])
 
 @app.route('/update_git')
 def update_git():
   store.update_versions()
   return "OK"
+
+@app.route('/update_version', methods=['POST'])
+@basic_auth.required
+def update_version():
+  if request.is_json:
+    data = request.get_json()
+    print(data)
+    try: # sanity check just a wee bit
+      for kit, kit_info in data['kits'].items():
+        print("Updating kit \"" + kit + "\".")
+        for release, release_info in data['kits'][kit]['releases'].items():
+          if 'version' in release_info:
+            continue
+    except Exception as err:
+      raise
+      print("Received invalid update payload.")
+      return "MALFORMED", 400
+
+    store.update_data(data)
+    return "OK"
+  return "BAD", 400
   
 
 if __name__ == "__main__":
   store.load_config()
   if not DEBUG:
-    # automatically load config if not debugging. this is to prevent
+    # only auto load config if not debugging. this is to prevent
     # massive API hits caused each time a src file edit is saved to disk.
     # to update dependency info while developing, hit the `/update_git`
     # endpoint
--- models/data.py
+++ models/data.py
@@ -1,4 +1,4 @@
-import json, os, sys, yaml
+import json, os, sys, yaml, collections
 from threading import Lock
 from .fetcher import get_latest
 
@@ -25,28 +25,29 @@ class Data():
       # ensure datastore isn't updated while writing to disk
       with self.data_mutex:
         with open(self.config_file, 'w') as config:
-          config.write(yaml.safe_dump(self.internal_data))
+          config.write(yaml.safe_dump(self.internal_data, default_flow_style=False))
           print("Successfully saved configuration to disk.")
 
   def update_data(self, data: dict):
     with self.data_mutex:
-      self.internal_data.update(data)
+      dict_merge(self.internal_data, data)
+    self.write_config()
 
   def update_versions(self):
     seen_releases = {} # cache releases that are the same across kits
     with self.data_mutex:
-      for k_index, kit in enumerate(self.internal_data['kits']):
-        for r_index, release in enumerate(kit['releases']):
+      for kit, kit_info in self.internal_data['kits'].items():
+        for release, release_info in kit_info['releases'].items():
           # check if we've seen this release before, if so,
           # then used cached version info (reduces GH API calls)
-          if release['git'] in seen_releases:
-            print("Using cached information for \"" + release['name'] + "\".")
-            self.internal_data['kits'][k_index]['releases'][r_index]['latest_version'] = seen_releases[release['git']]
+          if release_info['git'] in seen_releases:
+            print("Using cached information for \"" + release + "\".")
+            self.internal_data['kits'][kit]['releases'][release]['latest_version'] = seen_releases[release_info['git']]
           else:
-            print("Checking release \"" + release['name'] + "\".")
-            latest_release = get_latest(release['git'])
-            seen_releases[release['git']] = latest_release
-            self.internal_data['kits'][k_index]['releases'][r_index]['latest_version'] = latest_release
+            print("Checking release \"" + release + "\".")
+            latest_release = get_latest(release_info['git'])
+            seen_releases[release_info['git']] = latest_release
+            self.internal_data['kits'][kit]['releases'][release]['latest_version'] = latest_release
       print("Loaded dependencies: " + str(self.internal_data))
   
   def get_data(self) -> dict:
@@ -55,3 +56,18 @@ class Data():
 
 
 
+def dict_merge(dct, merge_dct):
+    """ Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
+    updating only top-level keys, dict_merge recurses down into dicts nested
+    to an arbitrary depth, updating keys. The ``merge_dct`` is merged into
+    ``dct``.
+    :param dct: dict onto which the merge is executed
+    :param merge_dct: dct merged into dct
+    :return: None
+    """
+    for k, v in merge_dct.items():
+        if (k in dct and isinstance(dct[k], dict)
+                and isinstance(merge_dct[k], collections.Mapping)):
+            dict_merge(dct[k], merge_dct[k])
+        else:
+            dct[k] = merge_dct[k]
